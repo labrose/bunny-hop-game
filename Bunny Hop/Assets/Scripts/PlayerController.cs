ccc2decb5350ff5d2614792b1e899fe43ee4180d
Lauren Brose <labrose@github.com>
2019-11-08 21:20:05 -0800

    Added basic player movement

--- Bunny Hop/Assets/Scripts/PlayerController.cs
+++ Bunny Hop/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody playerRb;
+
+    private bool isOnGround;
+    private float speed = 10f;
+    private float jumpForce = 10f;
+    private float gravityModifier = 5f;
+
+    public bool isGameOver;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the player inputs
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+
+        // Move the player left / right
+        playerRb.AddForce(Vector3.forward * speed * verticalInput);
+        playerRb.AddForce(Vector3.right * speed * horizontalInput);
+
+        // Jump the player
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+
+        // Keep player in bounds
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Handle colliding on the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        // Handle colliding with food
+
+        // Handle colliding with enemy
     }
 }
